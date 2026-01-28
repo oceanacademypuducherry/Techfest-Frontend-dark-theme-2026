aa03cf6452b798c402966ac0a40ae3bb3e1ab18f
vidhya <vidhyavidhya200006@gmail.com>
2026-01-28 14:59:22 +0530

    changes after meeting

--- src/service/apiConfig.ts
+++ src/service/apiConfig.ts
@@ -3,9 +3,11 @@ import axios from "axios";
 export const UserAPI = axios.create({
   // baseURL:
   //   "http://127.0.0.1:5001/oceanlivereact/asia-south1/techfest_test_api/app",
-  // baseURL: "https://techfest-api-y5x6yhhkmq-el.a.run.app/app",
+  baseURL: "https://techfest-api-y5x6yhhkmq-el.a.run.app/app",
   // baseURL:"https://techfest-test-api-y5x6yhhkmq-el.a.run.app/app"
-  baseURL: "https://techfest-test-api-y5x6yhhkmq-el.a.run.app/app",
+  // baseURL: "https://techfest-test-api-y5x6yhhkmq-el.a.run.app/app",
+  // baseURL:"http://127.0.0.1:5001/oceanlivereact/asia-south1/techfest_api/app"
+  // baseURL:"https://asia-south1-oceanlivereact.cloudfunctions.net/techfest_api/app"
 });
 
 export default UserAPI;
